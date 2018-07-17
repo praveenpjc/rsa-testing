21dd159605fc6f538e7fc1bb00d36560528ee4a7
praveenpjc <40392793+praveenpjc@users.noreply.github.com>
2018-07-17 12:39:12 +0530

    Add files via upload

---
+++ testingitup.php
@@ -0,0 +1,19 @@
+echo Nav::widget([
+    'options' => ['class' => 'navbar-nav navbar-right'],
+    'items' => [
+        ['label' => 'Home', 'url' => ['/site/index']],
+        [
+           'label' => 'Status',
+           'items' => [
+                ['label' => 'Create', 'url' => ['/status/create']],
+            ],
+        ],                    
+        ['label' => 'About', 'url' => ['/site/about']],
+        ['label' => 'Contact', 'url' => ['/site/contact']],
+        Yii::$app->user->isGuest ?
+            ['label' => 'Login', 'url' => ['/site/login']] :
+            ['label' => 'Logout (' . Yii::$app->user->identity->username . ')',
+                'url' => ['/site/logout'],
+                'linkOptions' => ['data-method' => 'post']],
+    ],
+]);
